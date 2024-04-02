7269c6291b1a30a3a745031162fd2bb9c3fb8be2
Erhan Güney <erhangyneus@gmail.com>
2024-04-02 13:57:01 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	qrcode "github.com/skip2/go-qrcode"
+)
+
+func main() {
+	var filename, url string
+
+	fmt.Println("QR Code oluşturmak istediğiniz bağlantıyı girin:")
+	fmt.Scan(&url)
+	fmt.Println("QR Code kaydetmek istediğiniz ismi girin:")
+	fmt.Scan(&filename)
+	filename = filename + ".png"
+	qrcode.WriteFile(url, qrcode.Medium, 256, filename)
+
+}
